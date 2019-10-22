10bfe9a93768ba506556549c0740532622000145
Tory Stosse <tory.mae.stosse@gmail.com>
2019-10-22 10:03:58 -0400

    updated game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,27 +1,23 @@
-console.log("Up and running!");
 
 
-
-var cards = ["queen", "queen", "king", "king"];
+const cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0]
-cardsInPlay.push(cardOne);
 
-var cardTwo = cards[2]
-cardsInPlay.push(cardTwo);
-
-
-console.log("User flipped queen");
-console.log("User flipped king");
-
-if (cardsInPlay.length === 2) {
+function checkForMatch() {
+	if (cardsInPlay.length === 2) {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again.");
+			console.log("You found a match!");
+		} else {
+			console.log("Sorry, try again.");
+		}
 	}
-}
+};
 
+function flipCard(cardID) {
+	cardsInPlay.push(cards[cardID]);
+	console.log("User flipped " + cards[cardID]);
+	checkForMatch();
+};
 
-
-
+flipCard(0);
+flipCard(2);
