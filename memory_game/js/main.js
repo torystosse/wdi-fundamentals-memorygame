2a54a66bcc89fad9ff8fce08c6d1dfeada0abc11
Tory Stosse <tory.mae.stosse@gmail.com>
2019-10-23 11:00:47 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,21 +1,45 @@
 
 
-const cards = ["queen", "queen", "king", "king"];
+const cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
+
 var cardsInPlay = [];
 
 function checkForMatch() {
 	if (cardsInPlay.length === 2) {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-			console.log("You found a match!");
+			alert("You found a match!");
 		} else {
-			console.log("Sorry, try again.");
+			alert("Sorry, try again.");
 		}
 	}
 };
 
 function flipCard(cardID) {
-	cardsInPlay.push(cards[cardID]);
-	console.log("User flipped " + cards[cardID]);
+	cardsInPlay.push(cards[cardID].rank);
+	console.log("User flipped " + cards[cardID].rank);
+	console.log("User flipped " + cards[cardID].suit);
+	console.log("User flipped " + cards[cardID].cardImage);
 	checkForMatch();
 };
 
