211206c6dd29b819855d6bf6f2d019ecac50db97
Tory Stosse <tory.mae.stosse@gmail.com>
2019-10-24 10:30:15 -0400

    completed game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -25,6 +25,7 @@ const cards = [
 
 var cardsInPlay = [];
 
+
 function checkForMatch() {
 	if (cardsInPlay.length === 2) {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -35,13 +36,24 @@ function checkForMatch() {
 	}
 };
 
-function flipCard(cardID) {
-	cardsInPlay.push(cards[cardID].rank);
-	console.log("User flipped " + cards[cardID].rank);
-	console.log("User flipped " + cards[cardID].suit);
-	console.log("User flipped " + cards[cardID].cardImage);
+function flipCard() {
+	var cardId = this.getAttribute('data-id');
+	this.setAttribute('src', cards[cardId].cardImage);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log("User flipped " + cards[cardId].suit);
+	console.log("User flipped " + cards[cardId].cardImage);
 	checkForMatch();
 };
 
-flipCard(0);
-flipCard(2);
+function createBoard () {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	};
+};
+
+createBoard();
